62ac8afe25e43042f18e81acfcdae2359e047a6b
김건학 <kkh10002@naver.com>
2024-11-09 15:59:00 +0900

    feat(inventory): 인밴토리 기능 구현
    상품을 통해 인벤토리를 생성한다.
    - 중복된 상품이 있는지 확인한다.
    
    일반 상품을 업데이트한다.
    - 상품을 찾고, 주문 수량만큼 재고가 존재하는지 확인한다.
    - 재고가 존재하면 수량을 업데이트한다.
    
    프로모션 상품을 업데이트한다.
    - 수량이 존재하면 해당 수량만큼 재고가 존재하는지 확인한다.
    - 재고가 존재하면 수량을 업데이트한다.
    
    이름, 프로모션이 같은 상품을 찾는다.
    - 둘다 없으면 에러가 발생한다.

--- src/main/java/domain/inventory/Inventory.java
+++ src/main/java/domain/inventory/Inventory.java
@@ -2,14 +2,14 @@ package domain.inventory;
 
 import domain.product.Product;
 import domain.product.Products;
+import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class Inventory {
     private final Set<Product> products;
 
-    private Inventory(final List<Product> products) {
+    private Inventory(final Set<Product> products) {
         this.products = validateDuplicateProduct(products);
     }
 
@@ -17,30 +17,43 @@ public class Inventory {
         return new Inventory(products.getProducts());
     }
 
-    public void updateInventory(final Product purchaseProduct) {
-        Product findProduct = findPurchaseProduct(purchaseProduct);
-        checkProductStockAvailability(findProduct, purchaseProduct);
-        reduceInventory(findProduct, purchaseProduct);
+    public void updateInventoryForRegular(final Product purchaseProduct) {
+        Product findProduct = findProductByNameAndPromotion(purchaseProduct);
+        checkRegularProductStockAvailability(findProduct, purchaseProduct);
+        reduceProductStock(findProduct, purchaseProduct);
     }
 
-    public Product findPurchaseProduct(final Product purchaseProduct) {
+    public void updateInventoryForPromotion(final Product purchaseProduct) {
+        Product findProduct = findProductByNameAndPromotion(purchaseProduct);
+        checkPromotionProductStockAvailability(findProduct, purchaseProduct);
+        reduceProductStock(findProduct, purchaseProduct);
+    }
+
+    public Product findProductByNameAndPromotion(final Product purchaseProduct) {
         return products.stream()
-                .filter(product -> isMatchingProduct(product, purchaseProduct))
+                .filter(product -> product.isSameName(purchaseProduct.getName()))
+                .filter(purchaseProduct::equals)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요."));
     }
 
-    private void checkProductStockAvailability(final Product findProduct, final Product purchaseProduct) {
-        if (findProduct.getQuantity() - purchaseProduct.getQuantity() < 0) {
-            throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+    private void checkRegularProductStockAvailability(final Product findProduct, final Product purchaseProduct) {
+        if (findProduct.getQuantity() < purchaseProduct.getQuantity()) {
+            throw new IllegalArgumentException("[ERROR] 일반 상품 재고가 부족합니다.");
         }
     }
 
-    private void reduceInventory(final Product product, final Product purchaseProduct) {
+    private void checkPromotionProductStockAvailability(final Product findProduct, final Product purchaseProduct) {
+        if (findProduct.getQuantity() < purchaseProduct.getQuantity()) {
+            throw new IllegalArgumentException("[ERROR] 프로모션 상품 재고가 부족합니다.");
+        }
+    }
+
+    private void reduceProductStock(final Product product, final Product purchaseProduct) {
         product.reduceProductStock(purchaseProduct.getQuantity());
     }
 
-    private Set<Product> validateDuplicateProduct(final List<Product> products) {
+    private Set<Product> validateDuplicateProduct(final Set<Product> products) {
         Set<Product> deDuplicateProducts = new HashSet<>(products);
         if (deDuplicateProducts.size() != products.size()) {
             throw new IllegalArgumentException("[ERROR] 중복된 제품이 있어서는 안됩니다.");
@@ -49,7 +62,7 @@ public class Inventory {
         return deDuplicateProducts;
     }
 
-    private boolean isMatchingProduct(final Product product, final Product purchaseProduct) {
-        return product.equals(purchaseProduct) || product.isSameName(purchaseProduct.getName());
+    public Set<Product> getProducts() {
+        return Collections.unmodifiableSet(products);
     }
 }
--- src/test/java/domain/inventory/InventoryTest.java
+++ src/test/java/domain/inventory/InventoryTest.java
@@ -5,14 +5,17 @@ import domain.product.Product.Builder;
 import domain.product.Products;
 import domain.product.ProductsFactory;
 import domain.product.Quantity;
+import domain.promotion.Promotion;
 import domain.promotion.Promotions;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import utils.Parser;
 
 class InventoryTest {
     Promotions promotions;
+    Promotion twoPlusOne;
     Products products;
     Inventory inventory;
 
@@ -28,31 +31,68 @@ class InventoryTest {
             throw new RuntimeException(e);
         }
         inventory = Inventory.from(products);
+
+        twoPlusOne = Promotion.of("탄산2+1", 2, 1,
+                Parser.parseDate("2024-01-01"), Parser.parseDate("2024-12-31"));
     }
 
     @Test
-    void 같은_상품이_존재해서는_안된다() {
-        Product product = new Builder("사이다", Quantity.from(1)).build();
-        Product sameProduct = new Builder("사이다", Quantity.from(2)).build();
-        Products sameProducts = ProductsFactory.createProductsByProducts(Arrays.asList(product, sameProduct));
+    void 일반상품의_인밴토리를_업데이트한다() {
+        Product regularCoke = new Builder("콜라", Quantity.from(5)).build();
 
-        org.assertj.core.api.Assertions.assertThatThrownBy(() -> Inventory.from(sameProducts))
-                .isInstanceOf(IllegalArgumentException.class);
+        inventory.updateInventoryForRegular(regularCoke);
+
+        Product findProduct = inventory.findProductByNameAndPromotion(regularCoke);
+        Assertions.assertThat(findProduct.getQuantity()).isEqualTo(5);
+    }
+
+    @Test
+    void 프로모션상품의_인밴토리를_업데이트한다() {
+        Product promotionCocke = new Builder("콜라", Quantity.from(5)).promotion(twoPlusOne).build();
+
+        inventory.updateInventoryForRegular(promotionCocke);
+
+        Product findProduct = inventory.findProductByNameAndPromotion(promotionCocke);
+        Assertions.assertThat(findProduct.getQuantity()).isEqualTo(5);
+
     }
 
     @Test
     void 존재하지_않는_상품은_구매할_수_없다() {
         Product purchaseProduct = new Builder("민트초코", Quantity.from(1)).build();
 
-        org.assertj.core.api.Assertions.assertThatThrownBy(() -> inventory.updateInventory(purchaseProduct))
+        org.assertj.core.api.Assertions.assertThatThrownBy(() -> {
+                    inventory.findProductByNameAndPromotion(purchaseProduct);
+                })
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 재고_이상의_수량을_초과하여_구매할_수_없다() {
+    void 프로모션이나_일반상품_중_하나만_존재해도_제품을_찾을때_예외가_발생하지_않는다() {
+        Promotion mdRecommendPromotion = Promotion.of("MD추천상품", 1, 1,
+                Parser.parseDate("2024-01-01"), Parser.parseDate("2024-12-31"));
+        Product orangeJuice = new Builder("오렌지주스", Quantity.from(1)).promotion(mdRecommendPromotion).build();
+        Product water = new Builder("물", Quantity.from(1)).build();
+
+        org.junit.jupiter.api.Assertions.assertDoesNotThrow(() -> {
+            inventory.findProductByNameAndPromotion(orangeJuice);
+            inventory.findProductByNameAndPromotion(water);
+        });
+    }
+
+    @Test
+    void 일반상품에서_재고_이상의_수량을_초과하여_구매할_수_없다() {
         Product purchaseProduct = new Builder("물", Quantity.from(100)).build();
 
-        org.assertj.core.api.Assertions.assertThatThrownBy(() -> inventory.updateInventory(purchaseProduct))
+        org.assertj.core.api.Assertions.assertThatThrownBy(() -> inventory.updateInventoryForPromotion(purchaseProduct))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 프로모션상품에서_재고_이상의_수량을_초과하여_구매할_수_없다() {
+        Product purchaseProduct = new Builder("콜라", Quantity.from(100)).promotion(twoPlusOne).build();
+
+        org.assertj.core.api.Assertions.assertThatThrownBy(() -> inventory.updateInventoryForPromotion(purchaseProduct))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
